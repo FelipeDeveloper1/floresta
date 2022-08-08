2c4393640f956eff012b5f68013d8e89017bccce
garotodeprograma <110305250+LunarPhos@users.noreply.github.com>
2022-08-08 20:55:53 -0300

    Add files via upload

---
+++ Assets/global scripts/Ghost.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public float ghostDelay;
+    private float ghostDelaySeconds;
+    public GameObject ghost;
+	public bool makeGhost = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ghostDelaySeconds = ghostDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (makeGhost)
+            {
+            if(ghostDelaySeconds>0)
+            {
+                ghostDelaySeconds -= Time.deltaTime;
+            }
+            else 
+            {
+                GameObject currentGhost = Instantiate(ghost, transform.position, transform.rotation);
+                Sprite currentSprite = GetComponent<SpriteRenderer>().sprite;
+                currentGhost.transform.localScale = this.transform.localScale;
+                currentGhost.GetComponent<SpriteRenderer>().sprite = currentSprite;
+                ghostDelaySeconds = ghostDelay;
+                Destroy(currentGhost, 1f);
+            }
+        }
+    }
+}
